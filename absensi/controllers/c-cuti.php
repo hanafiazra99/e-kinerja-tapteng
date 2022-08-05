8fca28db4d83555491b0a9399bee94b5c4489bf4
Hanafi Azra <56179394+hanafiazra99@users.noreply.github.com>
2022-08-05 12:03:45 +0700

    hitung cuti pake function sql

--- absensi/controllers/c-cuti.php
+++ absensi/controllers/c-cuti.php
@@ -5,7 +5,6 @@ function page_title()
 {
 
     return 'Data Cuti';
-
 }
 
 
@@ -15,8 +14,9 @@ function portal_id()
 {
 
     return '38';
-
 }
+
+
 if (isset($_POST['TombolTambahCuti'])) {
     require "../../app/config.php";
     require "../../app/models.php";
@@ -28,19 +28,49 @@ if (isset($_POST['TombolTambahCuti'])) {
     }
 
     $proses_check = array();
-    $masa_waktu = explode("-",$_POST['masa_cuti']);
+    $masa_waktu = explode("-", $_POST['masa_cuti']);
     $masa_awal = str_replace("/", "-", $masa_waktu[0]);
     $masa_akhir = str_replace("/", "-", $masa_waktu[1]);
-    $data = array($_POST['pegawai'], $masa_awal, $masa_akhir, $_POST['keterangan']);
-    $field = array('pegawai', 'tgl_awal', 'tgl_akhir', 'keterangan');
-    array_push($proses_check, submit_data($koneksi, $data, $field, 'absensi_cuti'));
+    $tahun = explode('-', $masa_awal)[0];
+    $pegawai = $_POST['pegawai'];
+
+
+
+    $jatah_absen = 0;
+
+    for ($i = 1; $i <= 2; $i++) {
+        $conn = mysqli_query($koneksi, "SELECT absensi_jumlah_cuti_tahunan('" . $pegawai . "'," . ($tahun - $i) . ") as jumlah_absen");
+        $cek_jumlah_absen = mysqli_fetch_array($conn);
+        
+        if($cek_jumlah_absen['jumlah_absen'] == 0){
+            $jatah_absen = $jatah_absen +  $cek_jumlah_absen['jumlah_absen'];
+        }else{
+            break;
+        }
+        
+    }
+    
+    $jatah_absen += 12;
+    
+    $conn = mysqli_query($koneksi, "SELECT absensi_jumlah_cuti_tahunan('" . $pegawai . "'," . $tahun . ") as jumlah_absen");
+    $jumlah_absen = mysqli_fetch_array($conn);
+    
+    if ($jumlah_absen['jumlah_absen'] > $jatah_absen) {
+        pop_up_direct(BASE_URL, ABSENSI_URL . 'laporan/rekap-cuti/', 'Gagal', 'Cuti sudah lebih dari 12 dalam setahun.', 'modal-danger');
+    }
+    
+    
+
+    $data = array('', $_POST['pegawai'], $masa_awal, $masa_akhir, $_POST['keterangan']);
+    $field = array('id', 'pegawai', 'tgl_awal', 'tgl_akhir', 'keterangan');
+    array_push($proses_check, tambah_data($koneksi, $data, $field, 'absensi_cuti'));
 
     if (!in_array("fail", $proses_check)) {
         pop_up_direct(BASE_URL, ABSENSI_URL . 'laporan/rekap-cuti/', 'Berhasil', 'Berhasil mengubah data.', 'modal-success');
     } else {
         pop_up_direct(BASE_URL, ABSENSI_URL . 'laporan/rekap-cuti/', 'Gagal', 'Terjadi kesalahan pada sistem, laporkan ini pada pihak pengembang.', 'modal-danger');
     }
-}else if (isset($_POST['req'])) {
+} else if (isset($_POST['req'])) {
     if ($_POST['req'] == 'Change Select') {
         require "../../app/config.php";
         require "../../app/models.php";
@@ -48,6 +78,5 @@ if (isset($_POST['TombolTambahCuti'])) {
         require "../../app/component.php";
 
         echo component_select_option_response_ajax($koneksi, $_POST['table'], $_POST['id'], $_POST['data'], $_POST['where']);
-    } 
+    }
 }
-
